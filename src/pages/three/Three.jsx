ceec84764f6e4b1dee336e353cd42e0187349c0d
zhongyoufeng <l>
2018-11-30 11:51:17 +0800

    init

---
+++ src/pages/three/Three.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Three extends Component {
+  render() {
+    return (
+      <div id="three" className="page">
+        <h1>购物车</h1>
+      </div>
+    );
+  }
+}
